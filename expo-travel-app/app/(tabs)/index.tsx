c084c6be2fb14c8f49a7778446cbd829d0b91166
Jukunye <jukunyes@gmail.com>
2024-08-05 21:30:05 +0300

    feat: :art: Header customization

--- expo-travel-app/app/(tabs)/index.tsx
+++ expo-travel-app/app/(tabs)/index.tsx
@@ -1,11 +1,50 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
+import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
 
 const Page = () => {
   return (
-    <View style={Styles.container}>
-      <Text>Page</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: '',
+          headerLeft: () => (
+            <TouchableOpacity style={{ marginLeft: 20 }}>
+              <Image
+                source={{
+                  uri: 'https://xsgames.co/randomusers/avatar.php?g=female',
+                }}
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 10,
+                }}
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              style={{
+                marginRight: 20,
+                backgroundColor: Colors.white,
+                padding: 10,
+                borderRadius: 10,
+                shadowColor: '#171717',
+                shadowOffset: { width: 2, height: 4 },
+                shadowOpacity: 0.2,
+                shadowRadius: 3,
+                elevation: 4,
+              }}
+            >
+              <Ionicons name="notifications" size={20} color={Colors.black} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </>
   );
 };
 
